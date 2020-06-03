2fffe551e895cc0acbc810fda4e96632c47f6bd2
afalkon <falconfinance.alex@gmail.com>
2020-06-03 15:22:51 +0200

    Product Slider Section is added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
+
+    $(".rateyo").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true,
+  });
+
+ $('.product-slider__inner').slick({
+     slidesToShow: 4,
+     slidesToScroll: 4,
+     dots: true,
+     arrows: false,
+ });
+
+
     var mixer = mixitup('.products__inner-box');
 });
